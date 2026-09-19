879928029f8af81e88be25873bb66f77883ef7d7
mazenessam2005-crypto <mazenessam2005@gmail.com>
2026-09-19 10:55:23 +0300

    Implement course review API

--- server/src/routes/reviews.js
+++ server/src/routes/reviews.js
@@ -11,5 +11,14 @@ import {
 const router = Router();
 
 // TODO: wire up the routes described in README.md section 3.
+router.get('/summary', getCourseSummary);
+router.get('/', getAllReviews);
+router.get('/:id', getReview);
+
+router.post('/', createReview);
+
+router.put('/:id', updateReview);
+
+router.delete('/:id', deleteReview);
 
 export default router;
