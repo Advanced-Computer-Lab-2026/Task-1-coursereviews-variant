e2a32a2c9ccc27bdb2090e63fd4dc5aaeb3772f0
Youssef Omar Ahmed <youssefomar1.2005@gmail.com>
2026-09-19 13:03:18 +0300

    feat: update review.js

--- server/src/routes/reviews.js
+++ server/src/routes/reviews.js
@@ -10,6 +10,14 @@ import {
 
 const router = Router();
 
-// TODO: wire up the routes described in README.md section 3.
+// Static routes must precede dynamic parameterized routes
+router.get('/', getAllReviews);
+router.get('/summary', getCourseSummary);
+
+// Dynamic parameterized routes (:id)
+router.get('/:id', getReview);
+router.post('/', createReview);
+router.patch('/:id', updateReview);
+router.delete('/:id', deleteReview);
 
 export default router;
